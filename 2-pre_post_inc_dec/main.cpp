d144ffda4b26497673fe01a766edc9626e126430
Ayfer Şahin <56173267+ayfersahinn@users.noreply.github.com>
2022-08-07 22:26:16 +0300

    adding pre and post increment decrement

---
+++ 2-pre_post_inc_dec/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int num = 47;
+	
+	//post increment and pre increment
+	
+	num++;
+	cout<<num<<endl;
+	++num;
+	cout<<num<<endl;
+	num = num + 1;
+	cout<<num<<endl<<endl;
+	
+	//post decrement and pre decrement
+	
+	num--;
+	cout<<num<<endl;
+	--num;
+	cout<<num<<endl;
+	num = num -1;
+	cout<<num<<endl;
+	return 0;
+}
