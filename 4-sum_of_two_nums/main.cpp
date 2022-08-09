486b880310b434d66bf921e0a730c876cf6b51a0
Ayfer Şahin <56173267+ayfersahinn@users.noreply.github.com>
2022-08-09 22:05:22 +0300

    adding sum of two numbers

---
+++ 4-sum_of_two_nums/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int num1, num2, sum;
+	
+	cout<<"Please enter first number : ";
+	cin>>num1;
+	cout<<"Please enter second number : ";
+	cin>>num2;
+	
+	sum = num1 + num2;
+	
+	cout<<endl<<"Sum of two numbers: "<<sum;
+	return 0;
+}
