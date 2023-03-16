f6dadc7dac281ed77b81695ddbfbffb381c9f89e
Ayfer Şahin <ayfers547@gmail.com>
2023-03-16 23:10:37 +0300

    rename

---
+++ 1-pre_post_inc_dec/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int num = 47;
+	
+	//post increment and pre increment
+	
+	
+	cout<<num++<<endl;               //first, it prints the value then increases it. 
+	
+	cout<<++num<<endl;               //first, it increases the value then prints it.
+	
+	num = num + 1;
+	cout<<num<<endl<<endl;
+	
+	//post decrement and pre decrement
+	
+	
+	cout<<num--<<endl;              //first, it prints the value after that decreases it. 
+	
+	cout<<--num<<endl;              //first, it decreases the value after that prints it.
+	
+	num = num -1;
+	cout<<num<<endl;
+	
+	return 0;
+}
