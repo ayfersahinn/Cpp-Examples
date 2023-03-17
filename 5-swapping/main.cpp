5fa1282f502f5eb423f49b6990d7748956bd7cff
Ayfer Şahin <ayfers547@gmail.com>
2023-03-17 21:42:53 +0300

    swapping 2. method

---
+++ 5-swapping/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+//swapping without temp variable
+
+int main(){
+	
+	int a,b;
+	cout<<"Input first number: "<<endl;
+	cin>>a;
+	cout<<"Input second number: "<<endl;
+	cin>>b;
+	
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	
+	cout<<"after swapping the first number: "<<a<<endl;
+	cout<<"after swapping the second number: "<<b<<endl;
+	return 0;
+}
